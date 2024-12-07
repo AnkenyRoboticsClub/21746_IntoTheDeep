a23cc4ea3a279ee2d106c7338b3ed4389ab5ebf6
Gabriel Vesperman <90653423+Gabe2008@users.noreply.github.com>
2024-12-07 11:02:32 -0600

    auto wip without meep meep no done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAutonomous.java
@@ -31,24 +31,36 @@ public class SampleAutonomous extends LinearOpMode {
         TrajectoryActionBuilder traj1 = drive.actionBuilder(initialPose)
                 //simple movement, spline to a linear heading, so it will go to position
                 //(10, 10) with a heading of 90 degrees
-                .splineToLinearHeading(new Pose2d(10, 0, Math.toRadians(0)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(-30, 22, Math.toRadians(0)), Math.toRadians(0))
                 //move lift up after 1.5 seconds
                 /*.afterTime(1.5, lift.liftUp())*/;
 
         TrajectoryActionBuilder traj2 = traj1.endTrajectory().fresh()
                 //simple movement, spline to a linear heading, so it will go to it's original position
                 //(0,0) with a heading of 0 degrees
-                .splineToLinearHeading(new Pose2d(10, 10, Math.toRadians(0)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(-50, 23, Math.toRadians(0)), Math.toRadians(0))
                 //move lift down after 5 inches traveled
                 /*.afterDisp(5.0, lift.liftDown())*/;
 
         TrajectoryActionBuilder traj3 = traj2.endTrajectory().fresh()
-                .splineToLinearHeading(new Pose2d(0, 10, Math.toRadians(0)), Math.toRadians(0));
+                .splineToLinearHeading(new Pose2d(-50, 32, Math.toRadians(0)), Math.toRadians(0));
 
         TrajectoryActionBuilder traj4 = traj3.endTrajectory().fresh()
-                .splineToLinearHeading(new Pose2d(0, 0, Math.toRadians(0)), Math.toRadians(0));
+                .splineToLinearHeading(new Pose2d(-2, 32, Math.toRadians(0)), Math.toRadians(0));
         // actions that need to happen on init; for instance, a claw tightening.
         //Actions.runBlocking(claw.closeClaw());
+
+        TrajectoryActionBuilder traj5 = traj4.endTrajectory().fresh()
+                .splineToLinearHeading(new Pose2d(-50, 32, Math.toRadians(0)), Math.toRadians(0));
+
+        TrajectoryActionBuilder traj6 = traj5.endTrajectory().fresh()
+                .splineToLinearHeading(new Pose2d(-45, 42, Math.toRadians(0)), Math.toRadians(0));
+
+        TrajectoryActionBuilder traj7 = traj6.endTrajectory().fresh()
+                .splineToLinearHeading(new Pose2d(-2, 42, Math.toRadians(0)), Math.toRadians(0));
+
+        TrajectoryActionBuilder traj8 = traj7.endTrajectory().fresh()
+                .splineToLinearHeading(new Pose2d(-40, 47, Math.toRadians(0)), Math.toRadians(0));
 
         //wait for autonomous to start
         waitForStart();
@@ -62,7 +74,11 @@ public class SampleAutonomous extends LinearOpMode {
                         traj1.build(),
                         traj2.build(),
                         traj3.build(),
-                        traj4.build()
+                        traj4.build(),
+                        traj5.build(),
+                        traj6.build(),
+                        traj7.build(),
+                        traj8.build()
                 )
         );
     }
