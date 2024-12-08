180fbd13a8f1a26b7dedca948ca1504492175823
Gabriel Vesperman <90653423+Gabe2008@users.noreply.github.com>
2024-12-07 19:08:16 -0600

    made a new path in the meep meep tester and need to add it to auto.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -9,19 +9,37 @@ import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
+        double startX = 14;
+        double startY = -63;
+        double topY = 55+startY;
+        double firstX = 31+startX;
+        double secondX = 42+startX;
+        double wallX = 47+startX;
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
+                .setConstraints(50, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(startX, startY, Math.toRadians(-90)))
+                        //.forward(0.1)
+                        /*.forward(30)
                         .turn(Math.toRadians(90))
                         .forward(30)
                         .turn(Math.toRadians(90))
                         .forward(30)
                         .turn(Math.toRadians(90))
                         .forward(30)
-                        .turn(Math.toRadians(90))
+                        .turn(Math.toRadians(90))*/
+                        .lineToLinearHeading(new Pose2d(20+startX, 30+startY, Math.toRadians(-90)))
+                        .splineToLinearHeading(new Pose2d(20+startX, topY, Math.toRadians(-90)), Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(firstX, topY, Math.toRadians(-90)), Math.toRadians(0))
+                        .lineToLinearHeading(new Pose2d(firstX, 5+startY, Math.toRadians(-90)))
+                        .lineToLinearHeading(new Pose2d(firstX, topY, Math.toRadians(-90)))
+                        .lineToLinearHeading(new Pose2d(secondX, topY, Math.toRadians(-90)))
+                        .lineToLinearHeading(new Pose2d(secondX, 5+startY, Math.toRadians(-90)))
+                        .lineToLinearHeading(new Pose2d(36+startX, topY, Math.toRadians(180)))
+                        .splineToLinearHeading(new Pose2d(wallX, topY, Math.toRadians(180)), Math.toRadians(0))
+                        .lineToLinearHeading(new Pose2d(wallX, 10+startY, Math.toRadians(180)))
+                        .splineToLinearHeading(new Pose2d(20+startX, 5+startY, Math.toRadians(180)), Math.toRadians(180))
                         .build());
 
 
