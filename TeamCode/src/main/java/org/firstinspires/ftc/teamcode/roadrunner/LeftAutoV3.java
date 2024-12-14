1891bcfda1e413035f0dd598e05436cfebcd85b5
Gabriel Vesperman <90653423+Gabe2008@users.noreply.github.com>
2024-12-14 15:14:13 -0600

    added a v3(not done)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/LeftAutoV3.java
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+
+@Config
+//Need the autnomous tag in order for it show up on driver station as an autonomous program
+// You can also set the name of the autonomous and the group
+@Autonomous(name = "LeftV3", group = "Autonomous")
+public class LeftAutoV3 extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        /*DcMotorEx leftFront, leftBack, rightBack, rightFront;
+
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);*/
+
+        //set the starting position
+        Pose2d initialPose = new Pose2d(-33, -63, Math.toRadians(-90));
+
+        //initialize our roadrunner drivetrain
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        //initialize claw and lift from our mechanisms file
+        //Mechanisms.Claw claw = new Mechanisms.Claw(hardwareMap);
+        //Mechanisms.Lift lift = new Mechanisms.Lift(hardwareMap);
+        Mechanisms.Arm arm = new Mechanisms.Arm(hardwareMap);
+        Mechanisms.Intake intake = new Mechanisms.Intake(hardwareMap);
+        Mechanisms.Wrist wrist = new Mechanisms.Wrist(hardwareMap);
+
+        TrajectoryActionBuilder traj1 = drive.actionBuilder(initialPose)
+                .strafeToLinearHeading(new Vector2d(-52, -52), Math.toRadians(45))
+                .afterTime(0, arm.armScoreLowFix())
+                .afterTime(0, wrist.foldOutWrist())
+                ;
+
+        TrajectoryActionBuilder traj2 = traj1.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(-25, -36), Math.toRadians(-20))
+                //.afterTime(0.1, arm.armCollect())
+                //.afterTime(0, wrist.foldOutWrist())
+                //.afterTime(0, intake.intakeCollect())
+                ;
+
+        TrajectoryActionBuilder traj3 = traj2.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(-32, -32), Math.toRadians(-20))
+                ;
+        TrajectoryActionBuilder traj4 = traj3.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(-52, -52), Math.toRadians(45))
+                //.afterTime(0, intake.intakeOff())
+                //.afterTime(0, arm.armScoreLow())
+                ;
+        TrajectoryActionBuilder traj5 = traj4.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(-36, -32), Math.toRadians(90))
+                .afterTime(0, wrist.foldInWrist())
+                .afterTime(0, arm.armCollapseFix())
+                ;
+
+        //wait for autonomous to start
+        waitForStart();
+
+        //check if stop is requested
+        if (isStopRequested()) return;
+
+        //run actions sequentially, so it will run each action in order
+        Actions.runBlocking(
+                new SequentialAction(
+                        traj1.build()
+                        , wrist.foldOutWrist()
+                        , intake.intakeDeposit()
+                        , traj2.build()
+                        , arm.armCollectFix()
+                        , wrist.foldOutWrist()
+                        , intake.intakeCollect()
+                        , traj3.build()
+                        , intake.intakeCollect()
+                        , intake.intakeOff()
+                        , arm.armScoreLowFix()
+                        , traj4.build()
+                        , intake.intakeDeposit()
+                        , traj5.build()
+                )
+        );
+    }
+}
